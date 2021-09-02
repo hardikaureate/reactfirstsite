7c75cf034080806daeda467e1fafd3df812ae6dc
hardikaureate <hardik.devani@aureatelabs.com>
2021-09-02 12:09:45 +0530

    upload js files

---
+++ src/ReactMenu/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-light text-center">
+                <p>
+                © {new Date().getFullYear()} AureateLabs. All Rights Reserved | Terms & Condition
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
